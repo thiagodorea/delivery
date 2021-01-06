8d65d25be3e2015225835f439cf06a5333435f09
Thiago Dorea <thiago.dorea@hotmail.com>
2021-01-06 11:06:03 -0300

    PUT /orders/{id}/delivered

--- backend/src/main/java/com/devsuperior/delivery/controllers/OrderController.java
+++ backend/src/main/java/com/devsuperior/delivery/controllers/OrderController.java
@@ -11,7 +11,9 @@ import com.devsuperior.delivery.services.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,5 +40,11 @@ public class OrderController {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
         //para retornar um 201 de created
         return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping("/{id}/delivered")
+    public ResponseEntity<OrderDTO> setDelivered(@PathVariable Long id){
+        OrderDTO dto = service.setDelivered(id);
+        return ResponseEntity.ok().body(dto);
     }
 }
