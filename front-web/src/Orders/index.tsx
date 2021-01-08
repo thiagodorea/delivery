ea65c69905d989021cf3123b52be1828351b9657
Thiago Dorea <thiago.dorea@hotmail.com>
2021-01-08 14:29:40 -0300

    Search order location

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,10 +1,28 @@
-import StepsHeader from './StepsHeader'
+import { useEffect, useState } from 'react'
 import './styles.css'
+import ProductsList from './ProductsList'
+import StepsHeader from './StepsHeader'
+import { OrderLocationData, Product } from './types';
+import { fetchProducts } from '../api';
+import OrderLocation from './OrderLocation';
 
 function Orders(){
+    //estado, sempre uma variavel e uma função, o useSatae inicia com uma lista vazia 
+    const [products, setProducts] = useState<Product[]>([]);
+    const [orderLocation, setOrderLocation] =  useState<OrderLocationData>();
+
+    //carregar os dados do banco de dados
+    useEffect(() => {
+        fetchProducts()
+            .then(response => setProducts(response.data))
+            .catch(error => alert ("Error ao carregar os dados do servidor, favor atualize a pagina." + error))
+    }, [])
+
     return(
         <div className="orders-container">
             <StepsHeader />
+            <ProductsList products={products} />
+            <OrderLocation onChangeLocation={location => setOrderLocation(location)}/>
         </div>
     )
 }
