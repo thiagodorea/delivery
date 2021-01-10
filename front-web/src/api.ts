bb76db7fb09cbdd6343758f2b919b6fc9edb4230
Thiago Dorea <thiago.dorea@hotmail.com>
2021-01-10 17:58:31 -0300

    Add env Vars

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { OrderPayload } from "./Orders/types";
 
-const API_URL = "https://dsdeliver-thiago.herokuapp.com";
+// const API_URL = "https://dsdeliver-thiago.herokuapp.com";
+const API_URL = process.env.REACT_APP_API_URL;
 const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
 
 export function fetchProducts() {
